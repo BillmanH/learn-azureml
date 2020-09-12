8cd4d0c0a8da9c0c0b71af470f579529da806694
William Harding <william.jeffrey.harding@gmail.com>
2020-09-11 17:29:06 -0700

    Pipeline running again.

--- config.py
+++ config.py
@@ -82,3 +82,18 @@ amlcompute_run_config = RunConfiguration(conda_dependencies=cd)
 amlcompute_run_config.environment.docker.enabled = True
 amlcompute_run_config.environment = Environment.get(
     ws, name='AzureML-AutoML').clone("bills-test")
+
+
+pipestep_cd = CondaDependencies.create(
+    pip_packages=[
+        "pandas",
+        "numpy",
+        "azureml-sdk[automl,interpret]",
+        "azureml-defaults",
+        "azureml-train-automl-runtime",
+        "pyyaml"
+    ],
+    conda_packages=["xlrd", "scikit-learn", "numpy", "pyyaml"]
+)
+pipestep_run_config = RunConfiguration(conda_dependencies=pipestep_cd)
+pipestep_run_config.environment.docker.enabled = True
